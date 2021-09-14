85fd536a2215483538463591462c251b1fd64c2f
utsavmalla <utsavmalla7@gmail.com>
2021-09-14 23:28:04 +0545

    Changes can insert multipule subjects to students

--- src/student_subjects/controller/index.ts
+++ src/student_subjects/controller/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Sequelize } from "sequelize";
+import { Op, Sequelize } from "sequelize";
 import { v4 as UUIDV4 } from 'uuid';
 import Student from "../../student/model";
 import Subject from "../../subjects/model";
@@ -10,8 +10,7 @@ import Student_Subjects from "../model";
 
 class Student_SubjectsController {
 
-    //---- add subject to student from id params and req body
-    async addsubject(req: Request, res: Response) {
+     async addsubject(req: Request, res: Response) {
 
         try {
             Student
@@ -43,24 +42,44 @@ class Student_SubjectsController {
                         });
 
                     } else {
-                        Subject
-                            .findOne({ attributes: ['id'], where: { guid: subject_ids } })
-                            .then(async (sub) => {
-                                const guid = UUIDV4();
-                                // if (!sub) {
 
-                                // }
+                        subject_ids.forEach((subIds: any) =>{
 
-                                if (sub) {
-                                    console.log(sub, std)
-                                    console.log("test")
-                                    const record = await Student_Subjects.create({ student_id: std.id, subject_id: sub.id, guid })
-                                    return res.json({ record, msg: 'stubject added to student' })
+                         
+                                Subject
+                                .findOne({ attributes: ['id'], where: { guid: subIds } })
+                                .then(async (sub) => {
+                                    const guid = UUIDV4();
+    
+                                    if (sub) {
 
+                                        const findmatch = await Student_Subjects.findOne({where:{
+                                            [Op.and]:[ {student_id: std.id,subject_id: sub.id }]
+                                        }
+                                        });
+                                        if(findmatch == null){
+                                            console.log(sub, std)
+                                            console.log("test")
+                                            const record = await Student_Subjects.create({ student_id: std.id, subject_id: sub.id, guid })
+                                            return res.json({ record, msg: 'stubject added to student' })
 
-                                }
+                                        }else{
+                                            return res.json({ msg: 'subjects already exist' })
 
-                            })
+                                        }
+                                        
+                                       
+    
+    
+                                    }
+    
+                                }).catch(function(err){
+                                    res.json(err.message)
+                                })
+
+                            
+                        })
+                      
 
                     }
 
@@ -71,6 +90,68 @@ class Student_SubjectsController {
 
         }
     }
+
+    //---- add subject to student from id params and req body
+    // async addsubject(req: Request, res: Response) {
+
+    //     try {
+    //         Student
+    //             .findOne({
+    //                 attributes: ['id'], where: { guid: req.params.id },
+    //                 include: [{
+    //                     model: Subject,
+    //                     as: 'subjects'
+    //                 }]
+    //             })
+    //             .then(async (std) => {
+
+    //                 const {subject_ids} = req.body
+
+
+    //                 if (!std) {
+    //                     return res.status(404).send({
+    //                         message: 'Student Not Found',
+    //                     });
+
+    //                 }
+
+    //                 if(!subject_ids) {
+    //                     const deleted = await Student_Subjects.update({ deleted: Sequelize.fn('now') }, { where: { student_id: std.id } })
+    //                     console.log("if prinf")
+
+    //                     return res.json({
+    //                         message: 'Subject Is Deleted from student',
+    //                     });
+
+    //                 } else {
+    //                     Subject
+    //                         .findOne({ attributes: ['id'], where: { guid: subject_ids } })
+    //                         .then(async (sub) => {
+    //                             const guid = UUIDV4();
+    //                             // if (!sub) {
+
+    //                             // }
+
+    //                             if (sub) {
+    //                                 console.log(sub, std)
+    //                                 console.log("test")
+    //                                 const record = await Student_Subjects.create({ student_id: std.id, subject_id: sub.id, guid })
+    //                                 return res.json({ record, msg: 'stubject added to student' })
+
+
+    //                             }
+
+    //                         })
+
+    //                 }
+
+    //             })
+
+    //     } catch (error) {
+    //         console.log(error)
+
+    //     }
+    // }
 
 
     ///-----view all students with subjects 
