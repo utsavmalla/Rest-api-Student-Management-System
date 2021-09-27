ac8a77b3aa63ab2f113109b9faebd8b3c2df4e97
utsavmalla <46053051+utsavmalla@users.noreply.github.com>
2021-09-27 12:26:10 +0545

    view student and subjects
    View student and subjects using Postgres DB function

--- dbfunction.sql
+++ dbfunction.sql
@@ -2,11 +2,9 @@
 
 -- DROP FUNCTION public.get_student_subjects(uuid);
 
---function to get student by id and there subjects
-
 CREATE OR REPLACE FUNCTION public.get_student_subjects(
 	student_ids uuid)
-    RETURNS TABLE(student_id integer, student_guid uuid, student_first_name character varying, student_last_name character varying, subject_id integer, subject_guid uuid, subject_code character varying, subject_name character varying) 
+    RETURNS TABLE(stdids integer, student_guid uuid, student_name character varying, student_lastname character varying, subjects json) 
     LANGUAGE 'plpgsql'
     COST 100
     VOLATILE PARALLEL UNSAFE
@@ -14,25 +12,18 @@ CREATE OR REPLACE FUNCTION public.get_student_subjects(
 
 AS $BODY$
 begin
-	return query 
-		SELECT 
-			students.id student_id,
-			students.guid student_guid,
-			students.firstname student_first_name,
-			students.lastname student_last_name,
-			subjects.id subject_id,
-			subjects.guid subject_guid,
-			subjects.code subject_code,
-			subjects.name subject_name
-		FROM
-			students 
-		INNER JOIN student_subjects 
-			ON student_subjects.student_id = students.id
-		INNER JOIN subjects 
-			ON student_subjects .subject_id = subjects .id
-		WHERE
-			students.guid = student_ids;
-		
+    RETURN QUERY 
+	SELECT id AS stdids,std.guid AS student_guid, std.firstname AS student_firstname,
+ 	std.lastname AS student_lastname, sub_array AS subjects
+	FROM   students  std
+	LEFT JOIN  (  -- or LEFT JOIN ?
+   	SELECT stdsub.student_id AS id, json_agg(
+   		json_build_object('subid', sub.id, 'code name',sub.code,'name',sub.name)) AS sub_array
+   FROM   student_subjects stdsub
+   JOIN   subjects sub  ON sub.id = stdsub.subject_id
+   GROUP  BY stdsub.student_id
+   ) sub USING (id)
+   WHERE std.guid = student_ids; 
 end;
 $BODY$;
 
